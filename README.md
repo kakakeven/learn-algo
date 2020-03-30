ede3ce4ad9a48dd6bebfb534d292b7e02455d11a
gaoyutao <gaoyutao@qiyi.com>
2020-03-30 13:56:24 +0800

    初始化项目结构

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# learn-algo
+### 数据结构与算法之美代码练习
+
+- [ ] 数组实现 `List`(ArrayList);
+- [ ] 链表实现 `List`(LinkedList);
---
+++ src/main/java/array/MyArrayList.java
@@ -0,0 +1,64 @@
+package array;
+
+/**
+ * ArrayList 的简易实现
+ */
+public class MyArrayList {
+
+    /**
+     * 实际存储的数据
+     */
+    private int[] data;
+
+    /**
+     * 数组长度
+     */
+    private int n;
+
+    /**
+     * 实际存储数目
+     */
+    private int count;
+
+    /**
+     * 初始构造方法
+     *
+     * @param capacity
+     */
+    public MyArrayList(int capacity) {
+        data = new int[capacity];
+        this.n = capacity;
+        this.count = 0;
+    }
+
+    public void insert() {
+
+    }
+
+    public boolean delete(int index) {
+        return false;
+    }
+
+    /**
+     * 查找指定索引的值
+     *
+     * @param index
+     * @return
+     */
+    public int find(int index) {
+        if (index < 0 || index >= count) {
+            return -1;
+        }
+        return data[index];
+    }
+
+    /**
+     * 打印所有元素
+     */
+    public void printAll() {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+}
