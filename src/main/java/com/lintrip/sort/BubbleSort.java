c470a207fb9e1fab59eedfe3d1f8e75eeec2b685
kakakeven <gaoyt2012@foxmail.com>
2021-04-14 14:34:23 +0800

    冒泡排序

---
+++ src/main/java/com/lintrip/sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.lintrip.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {3, 4, 1, 8, 7,9,10};
+        solution(array);
+    }
+
+    private static void solution(int[] array) {
+        int n = array.length;
+        if (array.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
